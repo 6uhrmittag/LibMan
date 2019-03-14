e06d17879913692a2fdd01a4327a5c6fc63416d0
Marvin Heimbrodt <marvin@6uhrmittag.de>
2019-03-14 15:10:06 +0100

    Set Titeltext

--- src/main/java/com/LibMan/Main.java
+++ src/main/java/com/LibMan/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/GUI.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("LibMan - Bibliotheksmanagement");
         //primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
